6afd22dcffb3032ade5979102c87655f2840804d
Tim Kersey <t@k-t.im>
2014-03-05 13:58:43 -0800

    Initial empty project

---
+++ Resume/RESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RESAppDelegate.h
+//  Resume
+//
+//  Created by Tim Kersey on 3/5/14.
+//  Copyright (c) 2014 Tim Kersey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Resume/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Resume
+//
+//  Created by Tim Kersey on 3/5/14.
+//  Copyright (c) 2014 Tim Kersey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RESAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RESAppDelegate class]));
+    }
+}
